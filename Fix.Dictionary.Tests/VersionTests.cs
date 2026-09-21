a02f6c34ed154679952e7013101d05d3381aad8d
sgolev <53184912+sgolev@users.noreply.github.com>
2026-09-21 17:30:04 +0300

    Populate DefaultApplVerID (1137) on FIXT.1.1 logon

--- Fix.Dictionary.Tests/VersionTests.cs
+++ Fix.Dictionary.Tests/VersionTests.cs
@@ -21,5 +21,20 @@ public class VersionTests
         Assert.AreEqual("FIXT.1.1", Dictionary.Versions.FIXT_1_1.BeginString);
         Assert.AreEqual("FIX.5.0SP2", Dictionary.Versions.FIX_5_0SP2.BeginString);
     }
-}
 
+
+    [TestMethod]
+    public void TestApplVerID()
+    {
+        Assert.AreEqual("2", Dictionary.Versions.FIX_4_0.ApplVerID);
+        Assert.AreEqual("4", Dictionary.Versions.FIX_4_2.ApplVerID);
+        Assert.AreEqual("6", Dictionary.Versions.FIX_4_4.ApplVerID);
+        Assert.AreEqual("9", Dictionary.Versions.FIX_5_0SP2.ApplVerID);
+    }
+
+    [TestMethod]
+    public void TestApplVerIDIsEmptyForTheTransportVersion()
+    {
+        Assert.AreEqual(string.Empty, Dictionary.Versions.FIXT_1_1.ApplVerID);
+    }
+}
